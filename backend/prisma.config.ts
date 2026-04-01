0603faa1452a9409b2532231ead4be5c229fa605
Param Khodiyar <paramkhodiyar1008@gmail.com>
2026-04-01 11:44:07 +0530

    Restructured project folders as per requirements

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -5,9 +5,9 @@ import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "db/schema.prisma",
   migrations: {
-    path: "prisma/migrations",
+    path: "db/migrations",
   },
   engine: "classic",
   datasource: {
