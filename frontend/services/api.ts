9dd1e69f5153156112ba515170e245cb5da11c3f
Param Khodiyar <paramkhodiyar1008@gmail.com>
2026-04-08 15:31:35 +0530

    Added host links support and cors management

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || (process.env.NODE_ENV === 'production' ? "https://kitchenos-mo2z.onrender.com/v1" : "/v1"),
+    baseURL: process.env.NEXT_PUBLIC_API_URL ? `${process.env.NEXT_PUBLIC_API_URL}/v1` : "/v1",
     headers: {
         "Content-Type": "application/json",
     },
