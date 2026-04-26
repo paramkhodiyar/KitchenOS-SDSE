bf714f446367fcf62f0b252753ff4b7c3ca1d798
Param Khodiyar <paramkhodiyar1008@gmail.com>
2026-04-26 20:56:36 +0530

    Implented the singleton principle on prisma pool creation

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -1,6 +1,16 @@
 import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis;
+
+const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({
+        log: ["query"],
+    });
+
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
