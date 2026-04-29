96a14255ee3079d5cd3babe60e85d45802b421d8
Pratiti Paul <paulpratiti23@gmail.com>
2026-04-29 21:13:28 +0530

    prevented query logs

--- backend/src/config/prisma.js
+++ backend/src/config/prisma.js
@@ -7,9 +7,7 @@ const globalForPrisma = globalThis;
 
 const prisma =
     globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ["query"],
-    });
+    new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
     globalForPrisma.prisma = prisma;
